8b29219daed7fbe6869cd39aaaa108c66d55b9e8
tianyiduzung <yijia1012230799@gmail.com>
2016-10-31 20:45:18 +0800

    test

---
+++ day07/Noname11.java
@@ -0,0 +1,29 @@
+class Student
+{
+	abstract void study();
+	void sleep()
+	{
+		System.out.println("sleep");
+	}
+}
+
+interface Smoking
+{
+	void smoke();
+
+}
+
+class  ZhangSan extends Student impelments Smoking
+{
+	void study(){ }
+}
+
+class Lisi
+
+class Noname11
+{
+	public static void main(String[] args)
+	{
+		System.out.println("");
+	}
+}
